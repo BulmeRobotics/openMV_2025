9bb0bb511bd76dc83b113aa43e88fa60994347bb
Thomas <162445189+ThomasRauch00@users.noreply.github.com>
2025-07-17 14:04:20 +0200

    Update cam_main.py, Salvador, befor run

--- cam_main.py
+++ cam_main.py
@@ -12,15 +12,20 @@ from machine import Pin, UART
 #Set used Pins
 alert_pin = Pin("P7", Pin.OUT)
 reset_pin = Pin("P8", Pin.IN)
+wall_pin = Pin("P9", Pin.IN) #Pin korrigieren
 
 #Set Detection Grid
-max_detection_val_right = 250
-max_detection_val_left = 80
-mid_detection_val_up = 30                           #Change to Set Top Boarder (Je niedrieger desto höher die Grenze)
-mid_detection_val_down = 240-mid_detection_val_up
+max_detection_val_right = 270
+max_detection_val_left = 60
+mid_detection_val_top = 30                           #Change to Set Top Boarder (Je niedrieger desto höher die Grenze)
+mid_detection_val_bottom = 190
+top_boarder_deactivated = False                      #Set to False, when Horizontal Boarder needed
+if top_boarder_deactivated:
+    mid_detection_val_bottom = 255
 
 #Variables
 center_positon_x = 0    #Position of the detectet Object on the X - Achses
+center_positon_y = 0    #Position of the detectet Object on the Y - Achses
 reset_time = 0
 CamTransmit = ""        #Safe of the detected Object for Handover
 Counter_Red = Counter_Green = Counter_Yellow = 0      #Letter Counter
@@ -35,14 +40,20 @@ labels = None
 data = False
 min_confidence = 0.6
 acknowledged = "X"
-top_boarder_deactivated = True                        #Set to False, when Top Boarder needed
+init_done = True
+noWallFlag = False
 
-#Colour Thresholds
+cam_debug_mode = False   #Start debugging mode, skipping all resets
+
+
+#Color Thresholds
 thresholds = [
-    (30, 100, 15, 127, 7, 127),    #generic_red_thresholds
-    (33, 100, -51, -19, 4, 43),     #generic_green_thresholds
-    (58, 90, -9, 10, 46, 70),      #generic_yellow_thresholds
+    (19, 66, 28, 68, 3, 55),      #red thresholds
+    (25, 62, -51, -20, -5, 46),    #green thresholds
+    (51, 79, -12, 5, 43, 70),      #yellow thresholds
 ]
+
+#time.sleep(2)
 
 #Camera initialize
 sensor.reset()
@@ -50,7 +61,7 @@ sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time=2000)
 sensor.set_auto_gain(False)
-sensor.set_auto_whitebal(False)
+sensor.set_auto_whitebal(True)
 clock = time.clock()
 
 #Initialize UART Interface
@@ -71,19 +82,35 @@ def check_reset():
     global stop_reset
     global reset_time
     global frame_counter
-   # stop_reset = False #######################TESTESTEST############
-    if reset_pin.value() == True:
+    global run
+    global noWallFlag
+    if reset_pin.value():
         alert_pin.value(0)
-        time.sleep(1.5)
+        #time.sleep(1.5)
         print("Reset")
         stop_reset = False
+        run = False
         frame_counter = 0
+        noWallFlag = True
+    elif reset_pin.value() == False & noWallFlag:
+        run = True
+        noWallFlag = False
+
 
 #Function to check the start condition from UART and sending X to acknowlage
 def check_start():
     global data
     global run
+    global Counter_Red
+    global Counter_Green
+    global Counter_Yellow
+    global Counter_Harmed
+    global Counter_Safe
+    global Counter_Unharmed
+    global frame_counter
+
     data = uart.read()
+
     if data is not None:
         print(data)
         if "A" in data:
@@ -94,21 +121,59 @@ def check_start():
             print("End")
             run = False
 
+            #Reset Counters
+            Counter_Red = Counter_Green = Counter_Yellow = 0
+            Counter_Harmed = Counter_Safe = Counter_Unharmed = 0
+            frame_counter = 0
+
+
+def detection_grid():
+    #Drawing detection - grid
+    img.draw_line(max_detection_val_right, 0 ,max_detection_val_right, 240 , 255, 1)
+    img.draw_line(max_detection_val_left, 0 ,max_detection_val_left, 240 , 255, 1)
+    if top_boarder_deactivated == False:
+        img.draw_line(0, mid_detection_val_top ,255, mid_detection_val_top , 255, 1)
+        img.draw_line(0, mid_detection_val_bottom ,255, mid_detection_val_bottom , 255, 1)
+
+
+def cams_init():
+    global run
+    global init_done
+    sensor.reset()
+    sensor.set_pixformat(sensor.RGB565)
+    sensor.set_framesize(sensor.QVGA)
+    sensor.skip_frames(time=2000)
+    sensor.set_auto_gain(False)
+    sensor.set_auto_whitebal(True)
+    run = False
+
+
 #Main Loop
 while True:
     clock.tick()
     img = sensor.snapshot()
+
+    #Cam initialised when Reset Pin high & Init unused
+    if (reset_pin.value() & init_done):
+        cams_init()
+
+    #Check Start & Reset Pin
     check_start()
     check_reset()
+
+    #Skipps all resets
+    if cam_debug_mode:
+        run = True
+        stop_reset = False
 
     #Query to start and stop the detection after handover or at thee End
     if run == True and stop_reset == False:
 
-        #Drawing detection - grid
-        img.draw_line(max_detection_val_right, 0 ,max_detection_val_right, 240 , 255, 1)
-        img.draw_line(max_detection_val_left, 0 ,max_detection_val_left, 240 , 255, 1)
-        if top_boarder_deactivated == False:
-            img.draw_line(0, mid_detection_val_up ,240, mid_detection_val_up , 255, 1)
+        #Stops Initialisation
+        init_done = False
+
+        #Drawing a detection-grid
+        detection_grid()
 
         #Start of color detection:
         for i, threshold in enumerate(thresholds):
@@ -124,19 +189,20 @@ while True:
                 center_x = math.floor(x + (w / 2))
                 center_y = math.floor(y + (h / 2))
                 center_positon_x = center_x
+                center_positon_y = center_y
 
                 #Top Border
-                if center_y >= mid_detection_val_up or top_boarder_deactivated == True:
+                if (center_y >= mid_detection_val_top and center_positon_y <= mid_detection_val_bottom) or top_boarder_deactivated == True:
 
                     #Colour Counter increases when detected
                     frame_counter += 1
-                    if i == 0:
+
+                    if i == 0 and center_positon_y <= mid_detection_val_bottom:
                         Counter_Red += 1
                     elif i == 1:
                         Counter_Green += 1
                     elif i == 2:
                         Counter_Yellow += 1
-
 
                 if blob.elongation() > 0.5:
                     img.draw_edges(blob.min_corners(), color=(255, 0, 0))
@@ -146,7 +212,6 @@ while True:
                 img.draw_cross(blob.cx(), blob.cy())
                 img.draw_keypoints(
                     [(blob.cx(), blob.cy(), int(math.degrees(blob.rotation())))], size=20
-
                 )
 
         #Start of the letter detection:
@@ -163,20 +228,22 @@ while True:
                     img.draw_rectangle(d.rect(), color=(0, 0, 255), thickness=2)
                     img.draw_string(10, 10, labels[i], color=(0, 0, 255), scale=3)
                     center_positon_x = center_x
-
+                    center_positon_y = center_y
                     #Top Border
-                    if center_y >= mid_detection_val_up or top_boarder_deactivated == True:
+                    if (center_y >= mid_detection_val_top and center_y <= mid_detection_val_bottom) or top_boarder_deactivated == True:
 
                         #Letter Counter increases when detected
                         frame_counter += 1
                         if labels[i] == "H":
                             Counter_Harmed += 1
-                            print(Counter_Harmed)
+                            #print(Counter_Harmed)
                         if labels[i] == "U":
                             Counter_Unharmed += 1
+                            #print(Counter_Unharmed)
                         if labels[i] == "S":
                             Counter_Safe += 1
-
+                            #print(Counter_Safe)
+                    #print("X Achse: ",center_x, "Y Achse: ",center_y)
         #Evaluation and Handover of most detected Object
         if (frame_counter >= 5):
             variables = {
@@ -267,7 +334,7 @@ while True:
     #Beginnt Line 115:
 
         #############################################
-        #img.draw_line(0, mid_detection_val_up ,240 ,mid_detection_val_up , 255, 1)
-        #img.draw_line(0, mid_detection_val_down ,240 ,mid_detection_val_down , 255, 1)
+        #img.draw_line(0, mid_detection_val_top ,240 ,mid_detection_val_top , 255, 1)
+        #img.draw_line(0, mid_detection_val_bottom ,240 ,mid_detection_val_bottom , 255, 1)
         #############################################
 
